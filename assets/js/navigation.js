d3451f887537fce4e44b245a0d0f2eff4f1e919d
Ngô Quang Trường <ngoquangtruong.professional@gmail.com>
2024-08-01 14:41:07 +0700

    0.0.7

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,14 +1,71 @@
+// document.getElementById("showNavigation").addEventListener("click", function () {
+//     document.getElementById("navigation").classList.add("show");
+//     document.getElementById("navigationOverlay").classList.add("show");
+//     document.getElementById("showNavigation").classList.add("hide");
+//     document.getElementById("menuLogo").classList.add("hide");
+// });
+
+// document.getElementById("hideNavigation").addEventListener("click", function () {
+//     document.getElementById("navigation").classList.remove("show");
+//     document.getElementById("showNavigation").classList.remove("hide");
+//     document.getElementById("menuLogo").classList.remove("hide");
+//     document.getElementById("navigationOverlay").classList.remove("show");
+// });
+
+window.addEventListener('scroll', function() {
+    var menu = document.querySelector('.menu');
+    var menuHeight = menu.offsetHeight;
+    var scrollPosition = window.scrollY || window.pageYOffset;
+
+    if (scrollPosition > menuHeight) {
+        menu.classList.add('fixed');
+    } else {
+        menu.classList.remove('fixed');
+    }
+});
+
 document.getElementById("showNavigation").addEventListener("click", function () {
-    document.getElementById("navigation").classList.add("show");
-    document.getElementById("navigationOverlay").classList.add("show");
-    document.getElementById("showNavigation").classList.add("hide");
-    document.getElementById("menuLogo").classList.add("hide");
+    var navigation = document.getElementById("navigation");
+    var overlay = document.getElementById("navigationOverlay");
+
+    if (!navigation.classList.contains("show")) {
+        navigation.classList.add("show");
+        overlay.classList.add("show");
+        document.getElementById("showNavigation").classList.add("hide");
+        document.getElementById("menuLogo").classList.add("hide");
+
+        // Thêm lớp hoạt hình khi hiển thị
+        navigation.classList.remove("swipeOutRight");
+        navigation.classList.add("swipeInRight");
+
+        // Đảm bảo overlay hiển thị
+        overlay.classList.remove("hide");
+        overlay.classList.add("show");
+    }
 });
 
 document.getElementById("hideNavigation").addEventListener("click", function () {
-    document.getElementById("navigation").classList.remove("show");
-    document.getElementById("showNavigation").classList.remove("hide");
-    document.getElementById("menuLogo").classList.remove("hide");
-    document.getElementById("navigationOverlay").classList.remove("show");
+    var navigation = document.getElementById("navigation");
+    var overlay = document.getElementById("navigationOverlay");
+
+    if (navigation.classList.contains("show")) {
+        navigation.classList.remove("swipeInRight");
+        navigation.classList.add("swipeOutRight");
+
+        overlay.classList.add("hide");
+
+        document.getElementById("showNavigation").classList.remove("hide");
+        document.getElementById("menuLogo").classList.remove("hide");
+
+        setTimeout(function() {
+            navigation.classList.remove("show");
+            // Đảm bảo overlay không còn lớp show
+            
+            overlay.classList.remove("show");
+        }, 1000);
+        
+    }
 });
+
+
 
