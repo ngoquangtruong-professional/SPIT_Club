07cf6b5966725e06f701c9717a78d9a364286059
Ngô Quang Trường <ngoquangtruong.professional@gmail.com>
2024-08-05 00:07:41 +0700

    0.1.0

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -6,7 +6,11 @@ window.addEventListener('scroll', function () {
     var navigation = document.getElementById("navigation");
 
     if (scrollPosition > menuHeight) {
-        menu.classList.add('fixed');
+        if (!navigation.classList.contains("show")) {
+            menu.classList.add('fixed');
+        } else {
+            menu.classList.remove('fixed');
+        }
     } else {
         menu.classList.remove('fixed');
     }
@@ -16,32 +20,56 @@ document.getElementById("showNavigation").addEventListener("click", function () 
     var navigation = document.getElementById("navigation");
     var overlay = document.getElementById("navigationOverlay");
 
-    navigation.classList.add("show");
-    overlay.classList.add("show");
+    if (!navigation.classList.contains("show")) {
+        navigation.classList.add("show");
+        overlay.classList.add("show");
 
-    document.getElementById("bottomButton").style.visibility = "hidden";
-    document.getElementById("topButton").style.visibility = "hidden";
-    
-    navigation.classList.remove("swipeOutRight");
-    navigation.classList.add("swipeInRight");
+        document.getElementById("bottomButton").style.visibility = "hidden";
+        document.getElementById("bottomButton").style.opacity = "0";
+        document.getElementById("topButton").style.visibility = "hidden";
+        document.getElementById("topButton").style.opacity = "0";
+        
+        document.getElementById("showNavigation").classList.add("hide");
+        document.getElementById("menuLogo").classList.add("hide");
 
-    overlay.classList.remove("hide");
-    overlay.classList.add("show");
+        navigation.classList.remove("swipeOutRight");
+        navigation.classList.add("swipeInRight");
+
+        overlay.classList.remove("hide");
+        overlay.classList.add("show");
+
+        document.querySelector('.menu').classList.remove('fixed');
+    }
 });
 
 document.getElementById("hideNavigation").addEventListener("click", function () {
     var navigation = document.getElementById("navigation");
     var overlay = document.getElementById("navigationOverlay");
 
-    navigation.classList.remove("swipeInRight");
-    navigation.classList.add("swipeOutRight");
-    overlay.classList.add("hide");
+    if (navigation.classList.contains("show")) {
+        navigation.classList.remove("swipeInRight");
+        navigation.classList.add("swipeOutRight");
+        overlay.classList.add("hide");
 
-    document.getElementById("bottomButton").style.visibility = "visible";
-    document.getElementById("topButton").style.visibility = "visible";
+        document.getElementById("bottomButton").style.visibility = "visible";
+        document.getElementById("bottomButton").style.opacity = "1";
+        document.getElementById("topButton").style.visibility = "visible";
+        document.getElementById("topButton").style.opacity = "1";
 
-    setTimeout(function () {
-        navigation.classList.remove("show");
-        overlay.classList.remove("show");
-    }, 750);
+        document.getElementById("showNavigation").classList.remove("hide");
+        document.getElementById("menuLogo").classList.remove("hide");
+
+        setTimeout(function () {
+            navigation.classList.remove("show");
+            overlay.classList.remove("show");
+
+            var scrollPosition = window.scrollY || window.pageYOffset;
+            var menuWrapper = document.querySelector('.menu-wrapper');
+            var menuHeight = menuWrapper.offsetHeight;
+
+            if (scrollPosition > menuHeight) {
+                document.querySelector('.menu').classList.add('fixed');
+            }
+        }, 500);
+    }
 });
